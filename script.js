cfcd6fd9e14a41a78b852afb923731ee79670de9
Juliana Alves <julianaalvesmattos21@gmail.com>
2024-04-19 17:05:20 -0300

    Criando as funcionalidades do programa

--- script.js
+++ script.js
@@ -9,3 +9,73 @@ const closeModal= () => {
 document.getElementById('cadastrarUsuario').addEventListener('click', openModal)
 
 document.getElementById('modalClose').addEventListener('click', closeModal)
+
+//Peguei as Informações
+function InformacoesDados() {
+    let informacoesUsuarios = []
+
+    const nome= document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const celular = document.getElementById("celular").value;
+    const cidade = document.getElementById("cidade").value;
+
+    const dadosUsuarios={
+        nome: nome,
+        email: email,
+        celular: celular,
+        cidade: cidade
+    }
+
+console.log(dadosUsuarios)
+}
+
+ //Para guardar valores    
+
+    if (localStorage.getItem("cadastrarUsuario")) {
+        //se sim, eu guardo as informações dentro do array
+        //parse => de JSON para object
+        informacoesUsuarios = JSON.parse(localStorage.getItem("cadastrarUsuario"));
+    }
+
+    informacoesUsuarios.push()
+
+    localStorage.setItem("cadastrarUsuario", JSON.stringify(informacoesUsuarios))
+   
+    
+
+// Carregar
+
+let informacoesUsuarios = [];
+
+if (localStorage.getItem("cadastroUsuario")) {
+    informacoesUsuarios = JSON.parse(localStorage.getItem("cadastrarUsuario"));
+}
+
+document.getElementById("cadastroUsuario").addEventListener('click', closeModal)
+   
+function MontarTabela(){
+
+    let tabela = document.getElementById('corpo-tabela');
+
+    let template = "";
+    
+    informacoesUsuarios.forEach(pessoa => {
+        template += `
+        <tr>
+                <td data-cell="nome" > ${pessoa.nome} </td>
+                <td data-cell="email" > ${pessoa.email} </td>
+                <td data-cell="celular" > ${pessoa.celular} </td>
+                <td data-cell="cidade"> ${pessoa.cidade} </td>
+                </tr>
+                `
+            })
+
+            tabela.innerHTML=template
+        }
+
+    function SalvarDados(){
+        localStorage.getItem("cadastrarUsuario")
+        window.location.reload();
+    
+    }
+
